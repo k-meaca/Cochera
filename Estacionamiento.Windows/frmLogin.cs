4cab49b7165e0303dd3ac0ea24c0bf3d6133b4b4
k-meaca <keevme@gmail.com>
2022-11-07 16:37:12 -0300

    Se realizo parte de la info de sesion.

--- Estacionamiento.Windows/frmLogin.cs
+++ Estacionamiento.Windows/frmLogin.cs
@@ -128,34 +128,38 @@ namespace Estacionamiento.Windows
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            if (! ValidarDatos())
-            {
-                return;
-            }
+            //if (! ValidarDatos())
+            //{
+            //    return;
+            //}
 
-            try
-            {
-                if (servicio.ValidarUsuario(txtUsuario.Text, txtPassword.Text))
-                {
-                    Usuario usuario = servicio.CrearUsuario(txtUsuario.Text, txtPassword.Text);
-                    IniciarSesion(usuario);
-                }
-            }
-            catch (UsuarioInvalidoExcepcion)
-            {
-                LabelConError(lblErrorUsuario, "Usuario o contraseña mal ingresados.");
-                LabelConError(lblErrorPassword, "Usuario o contraseña mal ingresados.");
-            }
-            catch (SqlException)
-            {
-                Mensajero.MensajeError("No se ha podido conectar con el servidor.");
-            }
-            catch (Exception)
-            {
-                Mensajero.MensajeError("Ha ocurrido algo inesperado. Comuniquese con el tecnico.");
-            }
+            //try
+            //{
+            //    if (servicio.ValidarUsuario(txtUsuario.Text, txtPassword.Text))
+            //    {
+            //        Usuario usuario = servicio.CrearUsuario(txtUsuario.Text, txtPassword.Text);
+            //        IniciarSesion(usuario);
+            //    }
+            //}
+            //catch (UsuarioInvalidoExcepcion)
+            //{
+            //    LabelConError(lblErrorUsuario, "Usuario o contraseña mal ingresados.");
+            //    LabelConError(lblErrorPassword, "Usuario o contraseña mal ingresados.");
+            //}
+            //catch (SqlException)
+            //{
+            //    Mensajero.MensajeError("No se ha podido conectar con el servidor.");
+            //}
+            //catch (Exception)
+            //{
+            //    Mensajero.MensajeError("Ha ocurrido algo inesperado. Comuniquese con el tecnico.");
+            //}
+
+            //TODELETE
+            Usuario usuario = servicio.CrearUsuario("admin", "admin");
+            IniciarSesion(usuario);
         }
-
+        
         private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = !Validador.LetraLoginValida(e.KeyChar);
--- Estacionamiento.Windows/frmPrincipal.cs
+++ Estacionamiento.Windows/frmPrincipal.cs
@@ -19,11 +19,57 @@ namespace Estacionamiento.Windows
 
         private Usuario usuarioSesion;
 
+
+        //CONSTRUCTOR
+
         public frmPrincipal(Usuario usuarioSesion)
         {
             InitializeComponent();
 
+            reloj.Start();
+
             this.usuarioSesion = usuarioSesion;
+
+            InfoSesion();
+        }
+
+        //METODOS
+
+        //PRIVADOS
+
+        private void InfoSesion()
+        {
+            lblUsuario.Text = "USUARIO: " + usuarioSesion.Nombre + " " + usuarioSesion.Apellido;
+
+            lblFecha.Text = "FECHA: " + DateTime.Now.ToShortDateString();
+
+            lblHora.Text = "HORA: " + DateTime.Now.ToShortTimeString();
+
+        } 
+
+        //METODOS
+
+
+        //EVENTOS
+
+        private void reloj_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = "HORA: " + DateTime.Now.ToShortTimeString();
+        }
+
+        private void imgSalir_MouseEnter(object sender, EventArgs e)
+        {
+            imgSalir.BackColor = Color.Red;
+        }
+
+        private void imgSalir_MouseLeave(object sender, EventArgs e)
+        {
+            imgSalir.BackColor = Color.Transparent;
+        }
+        private void imgSalir_Click(object sender, EventArgs e)
+        {
+            reloj.Stop();
+            Close();   
         }
     }
 }
